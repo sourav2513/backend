c4cf3e804851282ec7e5dc645c8a2ff9726c6fca
sourav.iss <sourav.iss2015@gmail.com>
2024-05-04 05:23:09 +0530

    Project boilerplate setup

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import { ApolloServer } from "@apollo/server"
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { startStandaloneServer } from "@apollo/server/standalone"
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+	resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  // introspection: true,
+  // uploads: false,
+  // playground: {
+  //   endpoint: `http://localhost:3200/graphql`,
+  //   settings: {
+  //     'editor.theme': 'light'
+  //   }
+  // }
+})
+ 
+//  const { url } = await startStandaloneServer(server)
+ // Ensure we wait for our server to start
+await server.start();
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  '/',
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    // context: async ({ req }) => ({ token: req.headers.token }),
+    context: async ({ req }) => ({ req }),
+  }),
+);
+
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+await connectDB();
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
