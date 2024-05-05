684d5cb0e401d73e24ace1a82470cd15f08a0704
sourav.iss <sourav.iss2015@gmail.com>
2024-05-05 14:55:25 +0530

    Transaction resolver added

--- typeDefs/transaction.typeDef.js
+++ typeDefs/transaction.typeDef.js
@@ -14,6 +14,7 @@ const transactionTypeDef = `#graphql
   type Query {
     transactions: [Transaction!]
     transaction(transactionId:ID!): Transaction
+    # TODO => Add category Statistics query
     # categoryStatistics: [CategoryStatistics!]
   }
 
